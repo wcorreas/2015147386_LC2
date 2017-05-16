639683fe5d9da0eea9389d134c3e5c33e001cf55
Willy Correa <wilder_correa@usmp.pe>
2017-05-15 19:53:03 -0500

    Entities 2

---
+++ 2015147386-SLN/2015147386-PER/EntityTypeConfigurations/ServicioConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2015147386_PER.EntityTypeConfigurations
+{
+    class ServicioConfiguración
+    {
+    }
+}
